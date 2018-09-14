ae81779167fa0d07b84225f0425848f12400a659
Milos <cosma203@gmail.com>
2018-09-14 21:20:56 +0800

    Add tests for GET /users/me and POST /users routes

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,31 +4,11 @@ const { ObjectID } = require('mongodb');
 
 const app = require('./../server');
 const Todo = require('./../models/todo');
+const User = require('./../models/user');
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: 'First test todo'
-  },
-  {
-    _id: new ObjectID(),
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-  }
-];
-
-beforeEach(function(done) {
-  Todo.remove({})
-    .then(function() {
-      Todo.insertMany(todos).then(function() {
-        done();
-      });
-    })
-    .catch(function(err) {
-      done(err.message);
-    });
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', function() {
   it('should create a new todo', function(done) {
@@ -192,6 +172,74 @@ describe('PATCH /todos/:id', function() {
         expect(res.body.completed).toBe(false);
         expect(res.body.completedAt).toNotExist();
       })
+      .end(done);
+  });
+});
+
+describe('GET /users/me', function() {
+  it('should return user if authenticated', function(done) {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect(function(res) {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated', function(done) {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect(function(res) {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
+
+describe('POST /users', function() {
+  it('should create a user', function(done) {
+    var email = 'example@example.com';
+    var password = '123mnb!';
+
+    request(app)
+      .post('/users')
+      .send({ email, password })
+      .expect(200)
+      .expect(function(res) {
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body._id).toExist();
+        expect(res.body.email).toBe(email);
+      })
+      .end(function(err) {
+        if (err) {
+          return done(err.message);
+        }
+
+        User.findOne({ email }).then(function(user) {
+          expect(user).toExist();
+          expect(user.password).toNotBe(password);
+          done();
+        });
+      });
+  });
+
+  it('should return validation error if request invalid', function(done) {
+    request(app)
+      .post('/users')
+      .send({ email: 'and', password: '123' })
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create user if email in use', function(done) {
+    request(app)
+      .post('/users')
+      .send({ email: users[0].email, password: 'Password123!' })
+      .expect(400)
       .end(done);
   });
 });
