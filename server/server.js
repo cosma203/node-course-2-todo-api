40c8e907cc2c013356b20cb4121405eb18786cfe
Milos <cosma203@gmail.com>
2018-09-15 14:53:07 +0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -50,6 +50,20 @@ app.post('/users', function(req, res) {
     });
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then(user => {
+      return user.generateAuthToken().then(token => {
+        res.header('x-auth', token).send(user);
+      });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
+
 app.get('/todos', function(req, res) {
   Todo.find({})
     .then(function(todos) {
